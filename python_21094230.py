5ef0ece77e70be58698f0932ba9383ace90d62d2
sunwayTim <137743545+sunwayTim@users.noreply.github.com>
2023-07-03 22:26:13 +0800

    Update python_21094230.py

--- python_21094230.py
+++ python_21094230.py
@@ -1,5 +1,29 @@
-"""
-deez nuts
-"""
+import random
+import datetime
+import os
 
-# darren lame
+random.randint(2, 5)
+datetime
+os
+
+with open("reservation_21094230.txt", "r+") as f:
+    contents = f.readlines()
+
+
+def mainProgram():
+    print(f"[1] Add reservation"
+          f"\n[2] Cancel reservation"
+          f"\n[3] Update/edit reservation"
+          f"\n[4] Display reservation"
+          f"\n[5] Generate meal recommendation"
+          f"\n[6] Exit")
+
+
+
+
+
+
+print(contents[1])
+
+for i in contents:
+    print(i)
