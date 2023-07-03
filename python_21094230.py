ff34c231359c3b2fb66e26f917fe8e2961664b9a
sunwayTim <137743545+sunwayTim@users.noreply.github.com>
2023-07-03 22:44:21 +0800

    Update python_21094230.py

--- python_21094230.py
+++ python_21094230.py
@@ -2,28 +2,52 @@ import random
 import datetime
 import os
 
-random.randint(2, 5)
-datetime
-os
 
+# Opens file containing reservation information
 with open("reservation_21094230.txt", "r+") as f:
     contents = f.readlines()
 
+# Opens file containing list of menu items
+with open("menuItems_21094230.txt", "r") as g:
+    menuItems = g.readlines()
+
 
 def mainProgram():
-    print(f"[1] Add reservation"
-          f"\n[2] Cancel reservation"
-          f"\n[3] Update/edit reservation"
-          f"\n[4] Display reservation"
-          f"\n[5] Generate meal recommendation"
-          f"\n[6] Exit")
+    retry = True
+    while retry == True:
+        print(f"[1] Add reservation"
+              f"\n[2] Cancel reservation"
+              f"\n[3] Update/edit reservation"
+              f"\n[4] Display reservation"
+              f"\n[5] Generate meal recommendation"
+              f"\n[6] Exit")
+        selection = int(input("Enter your selection: "))
+        match selection:
+            case 1:
+                addReservation()
+            case 6:
+                print("Program closed")
+                retry = False
 
 
+def addReservation():
+    print("deeznuts")
 
 
+def cancelReservation():
+    print("dozenuts")
 
 
-print(contents[1])
+def editReservation():
+    print("derenuts")
 
-for i in contents:
-    print(i)
+
+def displayReservation():
+    print("dennuts")
+
+
+def mealRecommendation():
+    print("nownuts")
+
+
+mainProgram()
