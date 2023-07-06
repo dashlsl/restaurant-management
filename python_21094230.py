bd8eee616b448fdd97390b7919b365e1b1ecc875
sunwayTim <137743545+sunwayTim@users.noreply.github.com>
2023-07-06 22:58:19 +0800

    Update python_21094230.py

--- python_21094230.py
+++ python_21094230.py
@@ -5,7 +5,7 @@ import os
 
 # Opens file containing reservation information
 with open("reservation_21094230.txt", "r+") as f:
-    contents = f.readlines()
+    resDetails = f.readlines()
 
 # Opens file containing list of menu items
 with open("menuItems_21094230.txt", "r") as g:
@@ -22,6 +22,7 @@ def mainProgram():
               f"\n[5] Generate meal recommendation"
               f"\n[6] Exit")
         selection = int(input("Enter your selection: "))
+        print("")
         match selection:
             case 1:
                 addReservation()
@@ -31,7 +32,22 @@ def mainProgram():
 
 
 def addReservation():
-    print("deeznuts")
+    retry = True
+    while retry == True:
+        print(f"Adding new reservation... ")
+        date = input("Enter date of reservation (YYYY-MM-DD): ")
+        slot = int(input("Enter slot to reserve (1-4): "))
+        name = input("Enter name for reservation: ")
+        email = input("Enter email for reservation: ")
+        phone = input("Enter phone number for reservation: ")
+        pax = int(input("Enter number of pax (maximum 4): "))
+        entry1 = f"{date}|Slot {slot}|{name}|{email}|{phone}|{pax}"
+        resDetails.append(entry1)
+        """with open("reservation_21094230.txt", "a") as f:
+            f.write(f"{entry1}\n")"""
+        retry = False
+    return
+
 
 
 def cancelReservation():
