2395f8b76de4728cdc3194e3e0b426d6ad4dee33
JetTheSwagger <darrenlew2004@gmail.com>
2023-07-07 15:32:45 +0800

    Update darren.py

--- darren.py
+++ darren.py
@@ -1,12 +1,22 @@
-print (f"hello world")
-### update/edit reservation
+#update/edit reservation
 with open("reservation_21094230.txt", "r+") as f:
     contents = f.readlines()
 
 def update():
-    for i in contents:
-        print(i)
-    change = input("Which line is yous? ")
-    repeat = True
-    while repeat:
-        print ("Fuck")
+    name = input("Enter your name for the reservation: ").upper()
+    reservationFound = []
+    reservationSelect = []
+    numFound = []
+
+    q = 0
+    #to append new reservation into the list
+    for reservations in reserve:
+        q += 1
+        reservations = reservations.split("|")
+        #is 2 because in the list 2 is the name of the reservation
+        if reservation[2] == name:
+            reservations.append(reservations)
+            numFound.append[q]
+    
+
+
