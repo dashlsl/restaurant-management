0af4e4b53d1c637d2f2064ef7019f269991acb74
sunwayTim <137743545+sunwayTim@users.noreply.github.com>
2023-07-10 08:14:01 +0800

    Update darren.py

--- darren.py
+++ darren.py
@@ -9,7 +9,7 @@ def update():
     numFound = []
 
     q = 0
-    #to append new reservation into the list
+    # to append new reservation into the list
     for reservations in reserve:
         q += 1
         reservations = reservations.split("|")
@@ -18,8 +18,8 @@ def update():
             reservations.append(reservations)
             numFound.append[q]
     
-    #to check if reservations found or not found
-    #to furthur edit the reservation if reservation is found
+    # to check if reservations found or not found
+    # to furthur edit the reservation if reservation is found
     if len(reservationFound) >= 1:
         print(f"Reservation found.")
         #emumerate is used to access both the index and the item of a sequence simultaneously
@@ -34,13 +34,13 @@ def update():
             print("Error, please input your number again.")
             numChosen = int(input("Enter the number of the reservation to update: "))
     
-        #to change the date of the reservations
+        # to change the date of the reservations
         dateReplace = input("Enter the new date of the reservation (DD-MM-YYYY): ")
         dateNew = datetime.datetime.strptime(dateReplace, '%d-%m-%Y').date()
         dateToday = dateNew - dateNow
         day = dateToday.days
         
-        #to let user change the date of reservations because the date chosen is less than 5 days
+        # to let user change the date of reservations because the date chosen is less than 5 days
         while day < 5:
             print(f"Error! Please make reservations at least 5 days in advance.\n")
             dateReplace = input(f"Select a date (dd-mm-yyyy): ")
@@ -55,5 +55,5 @@ def update():
     else:
         print(f"Reservation not found!")
 
-# Timonthy if u not using reserve and dateNow as ur variable pls tell me
+# Timothy if u not using reserve and dateNow as ur variable pls tell me
 # so i can change it according to yours
